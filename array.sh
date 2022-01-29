efb878643a85867e89fd14d049567d60b253ce3a
Atin777 <atinsarkar7@outlook.com>
2022-01-29 16:10:50 +0530

    Fifth Commit

---
+++ array.sh
@@ -0,0 +1,12 @@
+name[0]=jones
+name[1]=jerry
+name[2]=cerely
+name[3]=anne
+name[4]=sony
+
+echo "Hello, ${name[0]}"
+echo "Hello, ${name[2]}"
+
+echo "List of array values are, ${name[*]}"
+
+echo "List 2 array, ${name[@]}"
