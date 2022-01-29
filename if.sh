005622ac9870a26d0f404f7c9de202960a9d4e10
Atin777 <atinsarkar7@outlook.com>
2022-01-29 20:36:03 +0530

    Sixth Commit

---
+++ if.sh
@@ -0,0 +1,16 @@
+
+#if...if statement
+
+echo "First number :"
+read a
+echo "Second number :"
+read b
+if [ $a -eq $b ]
+ then
+	echo "Both values are equal"
+
+#if...else..fi statement
+else
+	echo "Both values are not equal"
+
+fi
