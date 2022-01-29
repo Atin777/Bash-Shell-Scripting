c47514d45198e1d0f0a6887a29c7c57773087367
Atin777 <atinsarkar7@outlook.com>
2022-01-29 13:18:31 +0530

    Fourth Commit

---
+++ svar.sh
@@ -0,0 +1,9 @@
+echo "Current File Name is $0"
+echo "First Parameter is $1"
+echo "Second Parameter is $2"
+echo "Third Parameter is $3"
+
+echo "Quoted Parameters $@"
+echo "Quoted Parameters $*"
+
+echo "Total count of Parameters $#"
