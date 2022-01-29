0dc8e21d501cbdb4265a4be76622a7e350ea87a0
Atin777 <atinsarkar7@outlook.com>
2022-01-29 12:24:45 +0530

    Third Commit

---
+++ name.sh
@@ -0,0 +1,25 @@
+echo "Hello, you are to Mr. James Nolan"
+
+Designation="CEO"
+readonly Designation
+Contact=9433356716
+readonly Contact
+
+echo "My Designation in the company: $Designation"
+echo "You can contact me at: $Contact"
+
+#echo "I am an Assistant Manager Jones"
+
+Desig="Assistant Manager"
+Con=9433801140
+
+#echo "My designation: $Desig"
+#echo "My contact: $Con"
+
+unset Desig
+
+echo "$Desig, $Con"
+
+echo "$Designation, $Contact"
+
+
